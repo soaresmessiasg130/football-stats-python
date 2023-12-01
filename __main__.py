1c3ef4eaa470374779daff04a773034a701a9b8a
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2023-11-30 22:55:19 -0300

    Add Script

--- __main__.py
+++ __main__.py
@@ -1,27 +1,23 @@
-from logging import root
 import tkinter as tk
 import threading
 import webbrowser
 import random
 
-import plotly.graph_objs as graph_objs
-import dash_html_components as html
+import plotly.graph_objs as go
 
-from dash import Dash, dcc
+from dash import Dash, dcc, html
 from dash.dependencies import Output, Input
 
 class DashThread(threading.Thread):
     def __init__(self, data_list):
         threading.Thread.__init__(self)
         self.data_list = data_list
+
         self.app = Dash(__name__)
 
         self.app.layout = html.Div(
             [
-                dcc.Graph(
-                    id = "live-graph", 
-                    animate = True,
-                ),
+                dcc.Graph(id = "live-graph", animate = True),
                 dcc.Interval(
                     id = "graph-update",
                     interval = 1 * 1000,
@@ -30,12 +26,11 @@ class DashThread(threading.Thread):
         )
 
         @self.app.callback(
-            Output("live-graph", "figure"), 
-            [Input("graph-update", "n_intervals")],
+            Output("live-graph", "figure"), [Input("graph-update", "n_intervals")]
         )
         def update_graph(n):
             data = [
-                graph_objs.Scatter(
+                go.Scatter(
                     x = list(range(len(self.data_list[symbol]))),
                     y = self.data_list[symbol],
                     mode = "lines+markers",
@@ -43,13 +38,11 @@ class DashThread(threading.Thread):
                 )
                 for symbol in self.data_list.keys()
             ]
-            
-            fig = graph_objs.Figure(data = data)
+            fig = go.Figure(data = data)
 
             fig.update_xaxes(range = [max(0, n - 120), n])
 
             return fig
-
 
     def run(self):
         self.app.run_server(debug = False)
@@ -63,13 +56,14 @@ class App:
         dash_thread = DashThread(self.data_list)
         dash_thread.start()
 
-        webbrowser.open("https://localhost:8050")
+        webbrowser.open("http://localhost:8050")
 
-        self.root.after(1000, self.generate_prices())
+        self.root.after(1000, self.generate_prices)
 
     def generate_prices(self):
         for symbol in self.data_list.keys():
             new_price = random.randint(1, 100)
+            print(new_price)
             self.data_list[symbol].append(new_price)
 
         self.root.after(1000, self.generate_prices)
---
+++ src/tests/gui-test.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+window.title("Football Stats")
+window.geometry("600x400")
+
+label = tk.Label(text = "Championships")
+
+label.grid(column = 0, row = 0)
+
+label2 = tk.Label(text = "KSAJLKAJSLSAJKA")
+
+label2.grid(column = 0, row = 0)
+
+window.mainloop()
